8a83ceac1ed57733546186b2785fda37af5e1aa7
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2021-02-01 02:09:25 +0900

    :sparkles: (frontend) Use nprogress

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,13 +1,18 @@
 import '@/assets/styles/index.scss'
 
+import NProgress from 'nprogress'
 import routes from 'vite-plugin-pages/client'
 import { ViteSSG } from 'vite-ssg'
 
 import App from '@/App.vue'
 
-export const createApp = ViteSSG(App, { routes }, () => {
-  // install all modules under `modules/`
-  // Object.values(import.meta.globEager('./modules/*.ts')).map((i) =>
-  //   i.install?.(ctx)
-  // )
+export const createApp = ViteSSG(App, { routes }, ({ isClient, router }) => {
+  if (isClient) {
+    router.beforeEach(() => {
+      NProgress.start()
+    })
+    router.afterEach(() => {
+      NProgress.done()
+    })
+  }
 })
