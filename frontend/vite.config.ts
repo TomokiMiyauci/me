f5f033db9faaa3e9453b0e8bde3eb7986c2730df
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2021-02-01 02:39:41 +0900

    :wrench: (frontend) Use auto import component

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,8 +3,11 @@ import vue from '@vitejs/plugin-vue'
 import Prism from 'markdown-it-prism'
 import { resolve } from 'path'
 import { defineConfig } from 'vite'
+import Components from 'vite-plugin-components'
+import ViteIcons, { ViteIconsResolver } from 'vite-plugin-icons'
 import Markdown from 'vite-plugin-md'
 import Pages from 'vite-plugin-pages'
+
 const config = defineConfig({
   alias: {
     '@': resolve(__dirname, 'src')
@@ -12,6 +15,11 @@ const config = defineConfig({
   plugins: [
     vue({
       include: [/\.vue$/, /\.md$/]
+    }),
+    Components({
+      customComponentResolvers: ViteIconsResolver({
+        componentPrefix: ''
+      })
     }),
     Pages({
       extensions: ['vue', 'md'],
@@ -28,7 +36,8 @@ const config = defineConfig({
       markdownItSetup(md) {
         md.use(Prism)
       }
-    })
+    }),
+    ViteIcons()
   ]
 })
 
