c03d5b13e81af2c8246344070b2f5d9e28cbec58
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2021-02-01 02:10:01 +0900

    :wrench: (frontend) Add whitelist class

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
 const { join } = require('path')
 const BASE_DIR = join(__dirname, 'src')
 const VUE_FILE = join('**', '*.vue')
@@ -6,7 +7,10 @@ const typography = require('@tailwindcss/typography')
 module.exports = {
   purge: {
     enabled: process.env.NODE_ENV === 'production',
-    content: [join(BASE_DIR, VUE_FILE), join(__dirname, '*.html')]
+    content: [join(BASE_DIR, VUE_FILE), join(__dirname, '*.html')],
+    options: {
+      safelist: ['prose', 'prose-sm', 'm-auto']
+    }
   },
   darkMode: false, // or 'media' or 'class'
   theme: {
