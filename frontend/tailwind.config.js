fad4bedd12499d5dd611d71f53cb23b39ad7ccec
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2021-02-02 23:46:22 +0900

    :wrench: (frontend) Apply dark mode

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,17 +2,22 @@
 const { join } = require('path')
 const BASE_DIR = join(__dirname, 'src')
 const VUE_FILE = join('**', '*.vue')
+const TS_FILE = join('**', '*.ts')
 const typography = require('@tailwindcss/typography')
 
 module.exports = {
   purge: {
     enabled: process.env.NODE_ENV === 'production',
-    content: [join(BASE_DIR, VUE_FILE), join(__dirname, '*.html')],
+    content: [
+      join(BASE_DIR, VUE_FILE),
+      join(BASE_DIR, TS_FILE),
+      join(__dirname, '*.html')
+    ],
     options: {
       safelist: ['prose', 'prose-sm', 'm-auto']
     }
   },
-  darkMode: false, // or 'media' or 'class'
+  darkMode: 'media',
   theme: {
     extend: {}
   },
