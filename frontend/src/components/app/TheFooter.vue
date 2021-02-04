58ea13029fa5df5accf259b3484b116fc153069b
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2021-02-05 02:21:04 +0900

    :truck: (frontend) Replace footer

---
+++ frontend/src/components/app/TheFooter.vue
@@ -0,0 +1,8 @@
+<template>
+  <footer class="mt-10 mb-6 prose m-auto opacity-50 flex">
+    <span class="text-sm">
+      {{ new Date().getFullYear() }} ©Tomoki Miyauchi</span
+    >
+    <div class="flex-auto" />
+  </footer>
+</template>
