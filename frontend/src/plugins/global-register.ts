c9dc926bd9357f16f3094cd7f69e21bd4a5a9134
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2021-02-06 18:52:55 +0900

    :sparkles: (frontend) Change global component

--- frontend/src/plugins/global-register.ts
+++ frontend/src/plugins/global-register.ts
@@ -1,12 +1,12 @@
 import { Plugin } from 'vue'
 
+import MarkdownResolver from '@/components/MarkdownResolver.vue'
 import Post from '@/components/Post.vue'
-import Posts from '@/components/Posts.vue'
 
 const plugin: Plugin = {
   install: (app) => {
     app.component('Post', Post)
-    app.component('Posts', Posts)
+    app.component('MarkdownResolver', MarkdownResolver)
   }
 }
 
