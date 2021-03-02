762133e4fdbd9c044555158ca16d3d07192f0c66
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2021-03-03 02:17:19 +0900

    :sparkles: (frontend) Add global components

--- frontend/src/plugins/global-register.ts
+++ frontend/src/plugins/global-register.ts
@@ -1,12 +1,17 @@
 import { Plugin } from 'vue'
 
+import Alert from '@/components/global/base/Alert.vue'
+import CodeBlock from '@/components/global/base/CodeBlock.vue'
+import CodeGroup from '@/components/global/base/CodeGroup.vue'
 import MarkdownResolver from '@/components/MarkdownResolver.vue'
 import Post from '@/components/Post.vue'
-
 const plugin: Plugin = {
   install: (app) => {
     app.component('Post', Post)
     app.component('MarkdownResolver', MarkdownResolver)
+    app.component('CodeBlock', CodeBlock)
+    app.component('CodeGroup', CodeGroup)
+    app.component('Alert', Alert)
   }
 }
 
