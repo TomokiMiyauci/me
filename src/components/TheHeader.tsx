b83e0a0f7cd658a6f9d58e45d4560207e518193f
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2021-06-23 13:00:36 +0900

    :sparkles:  Add head and foot navigation

--- src/components/TheHeader.tsx
+++ src/components/TheHeader.tsx
@@ -24,7 +24,7 @@ const LinkButton: FC<{ to: string; originalPath: string }> = ({
     <LocalizedLink
       to={to}
       language={locale}
-      className={`px-2 py-1 w-11 md:w-16 h-11 md:h-16 shadow text-[0.65rem] text-mini md:text-base md:px-3 flex rounded-full md:px-3 flex items-center justify-center flex-col ${
+      className={`px-2 hidden md:inline-flex py-1 w-16 h-16 shadow md:px-3 rounded-full items-center justify-center flex-col ${
         isActive(to) ? 'text-accent ring-1 ring-accent' : ''
       }`}
     >
@@ -62,17 +62,12 @@ const TheHeader: FC<{ originalPath: string; className?: string }> = ({
     <header
       className={`
       fixed
-      md:sticky
-      md:top-0
-      md:bottom-auto
-      md:mx-auto
+      top-0
+      border-b
       md:border-none
       w-full
-      z-50
-      bottom-0
       bg-gray-50
       dark:bg-blue-gray-900
-      border-t
       dark:border-gray-800 ${className}`}
     >
       <div
@@ -98,7 +93,7 @@ const TheHeader: FC<{ originalPath: string; className?: string }> = ({
           </LinkButton>
         </span>
 
-        <div className="flex space-x-3 md:space-x-8 items-center">
+        <div className="flex space-x-5 md:space-x-8 items-center">
           <div className="relative flex items-center">
             <button className="flex text-accent" onClick={toggleShow}>
               <Icon className="w-8 h-8" icon={translateIcon} />
@@ -114,7 +109,8 @@ const TheHeader: FC<{ originalPath: string; className?: string }> = ({
           dark:bg-blue-gray-800
           mt-2
           right-0
-          bottom-10
+          top-8
+
           shadow
           text-lg
           md:bottom-auto
