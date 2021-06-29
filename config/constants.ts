7f75a4197f1d6c23e6bc066ceec2a2e59fc7440c
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2021-06-29 22:38:15 +0900

    :chart_with_upwards_trend:  Enable google analitics

--- config/constants.ts
+++ config/constants.ts
@@ -1,1 +1,12 @@
+import { FirebaseOptions } from 'firebase/app'
 export const SITE_URL = 'https://miyauchi.dev/'
+
+export const firebaseOptions: FirebaseOptions = {
+  apiKey: 'AIzaSyAN_gLPDQgXzROBhfpoEq7g-Wek6FY1kJE',
+  authDomain: 'blorogue.firebaseapp.com',
+  projectId: 'blorogue',
+  storageBucket: 'blorogue.appspot.com',
+  messagingSenderId: '205944386882',
+  appId: '1:205944386882:web:f48733fc94e54f0de244db',
+  measurementId: 'G-DWBGDDWNR9'
+}
