25abf17a97794e136172a0d80edf30b8e27183f8
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2021-07-04 22:34:01 +0900

    :wrench:  Change storybook config

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,25 +1,37 @@
-import { StorybookConfig, CoreConfig } from '@storybook/core-common'
+import { StorybookConfig } from '@storybook/core-common'
+// import { UserConfig } from 'vite'
+// import { resolve } from 'path'
 
-type WeakenMap<T, K extends keyof T> = {
-  [P in keyof T]: P extends K ? any : T[P]
-}
+// type WeakenMap<T, K extends keyof T> = {
+//   [P in keyof T]: P extends K ? any : T[P]
+// }
 
-interface ExtendedCoreConfig extends WeakenMap<CoreConfig, 'builder'> {
-  builder: 'webpack4' | 'webpack5' | 'storybook-builder-vite'
-}
+// interface ExtendedCoreConfig extends WeakenMap<CoreConfig, 'builder'> {
+//   builder: 'webpack4' | 'webpack5' | 'storybook-builder-vite'
+// }
 
-interface ExtendedStorybookConfig extends WeakenMap<StorybookConfig, 'core'> {
-  core: ExtendedCoreConfig
-}
+// interface ExtendedStorybookConfig extends WeakenMap<StorybookConfig, 'core'> {
+//   core: ExtendedCoreConfig
+//   viteFinal?: (config: UserConfig) => UserConfig
+// }
 
-const config: ExtendedStorybookConfig & { core: { builder: string } } = {
-  stories: [
-    '../stories/**/*.stories.mdx',
-    '../src/components/**/*.stories.@(js|jsx|ts|tsx)'
+const config: StorybookConfig = {
+  stories: ['../src/components/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    'storybook-tailwind-dark-mode'
   ],
-  addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
   core: {
-    builder: 'storybook-builder-vite'
+    builder: 'webpack4'
+  },
+  webpackFinal: (config) => {
+    config.module.rules.push({
+      test: /\.scss$/,
+      use: ['style-loader', 'css-loader', 'postcss-loader', 'sass-loader']
+    })
+
+    return config
   }
 }
 
