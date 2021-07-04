5d4ea95ffe6886cd1d2a31d434e17d7c79fe6450
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2021-07-04 22:31:42 +0900

    :truck:  Rename Alert component

---
+++ src/components/Alert/Alert.stories.tsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import { ComponentStory, ComponentMeta } from '@storybook/react'
+
+import Alert from './'
+
+const meta: ComponentMeta<typeof Alert> = {
+  title: 'Alert',
+  component: Alert,
+
+  decorators: [(Story) => <Story />],
+
+  args: {
+    children: (
+      <>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel aliquam,
+        recusandae eum dolor alias laudantium reprehenderit dignissimos et modi
+        maiores optio quia ut fuga reiciendis sint sequi. Pariatur, repudiandae
+        repellendus.
+      </>
+    )
+  }
+}
+
+const Template: ComponentStory<typeof Alert> = (args) => <Alert {...args} />
+
+const Default = Template.bind({})
+const Warning = Template.bind({})
+
+Warning.args = {
+  type: 'warning'
+}
+
+export default meta
+export { Default, Warning }
