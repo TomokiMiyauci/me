476d7b86493481441509912260c8af4ec6657bef
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2021-07-07 10:49:33 +0900

    :children_crossing:  Add number counter

--- src/components/Comment/CommentCount/IconWithLoading.tsx
+++ src/components/Comment/CommentCount/IconWithLoading.tsx
@@ -1,13 +1,17 @@
-import React, { FC } from 'react'
+import React, { FC, CSSProperties } from 'react'
 import commentEditOutline from '@iconify-icons/mdi/comment-edit-outline'
 import { Icon } from '@iconify/react'
 import Placeholder from '../../Placeholder'
 
 import 'react-loader-spinner/dist/loader/css/react-spinner-loader.css'
 import Loader from 'react-loader-spinner'
-const IconWithLoading: FC<{ loading: boolean }> = ({ loading, children }) => {
+const IconWithLoading: FC<{
+  loading: boolean
+  className?: string
+  style?: CSSProperties
+}> = ({ loading, children, className, style }) => {
   return (
-    <div className="flex space-x-2 items-center">
+    <div className={`flex space-x-2 items-center ${className}`} style={style}>
       <Icon className="w-7 h-7 text-accent" icon={commentEditOutline} />
       <Placeholder
         placeholding={!loading}
