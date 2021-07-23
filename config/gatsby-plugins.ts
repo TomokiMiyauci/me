249b7f7be4c138f8ee3d06919a7abe50153a19a0
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2021-07-23 20:11:57 +0900

    :wrench:  Change offline plugin

--- config/gatsby-plugins.ts
+++ config/gatsby-plugins.ts
@@ -169,7 +169,12 @@ const plugins: GatsbyConfig['plugins'] = [
     }
   },
   'gatsby-plugin-sass',
-  'gatsby-plugin-offline'
+  {
+    resolve: `gatsby-plugin-offline`,
+    options: {
+      precachePages: ['/posts/', '/posts/*', '/ja/posts/', '/ja/posts/*']
+    }
+  }
   // 'gatsby-plugin-preact'
   // 'gatsby-plugin-webpack-bundle-analyser-v2'
 ]
