2b2d9841091868438ec943e1cfab1c6736f08c6b
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2021-07-28 22:27:01 +0900

    :lipstick:  Change logo breakpoint

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,14 +1,14 @@
 import React, { FC } from 'react'
 import { LocalizedLink, useLocalization } from 'gatsby-theme-i18n'
-import LL from '@/assets/logo.svg'
+import LogoSvg from '@/assets/logo.svg'
 
 const Logo: FC<{ shrink?: boolean }> = ({ shrink }) => {
   const { locale } = useLocalization()
   return (
     <LocalizedLink language={locale} className="text-4xl space-x-4" to="/">
-      <LL className="w-10 fill-current text-accent align-middle h-10" />
+      <LogoSvg className="w-10 fill-current text-accent align-middle h-10" />
 
-      <span className={`text-accent ${shrink ? 'hidden md:inline' : ''}`}>
+      <span className={`text-accent ${shrink && 'hidden lg:inline'}`}>
         miyauci.me
       </span>
     </LocalizedLink>
