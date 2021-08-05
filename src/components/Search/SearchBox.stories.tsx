40d6c84598af7daebca0f3de711e1da163fb2893
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2021-08-05 15:13:19 +0900

    :truck:  Rename it

---
+++ src/components/Search/SearchBox.stories.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { ComponentStory, ComponentMeta } from '@storybook/react'
+
+import SearchBox from '@/components/Search/SearchBox'
+
+const meta: ComponentMeta<typeof SearchBox> = {
+  title: 'Search/SearchBox',
+  component: SearchBox
+}
+
+const Template: ComponentStory<typeof SearchBox> = (args) => (
+  <SearchBox {...args} />
+)
+
+const Default = Template.bind({})
+
+export default meta
+export { Default }
