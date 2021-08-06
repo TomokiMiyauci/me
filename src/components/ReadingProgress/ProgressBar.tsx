9b344ab63a99187aba0e92c0d5d8e01d73de945a
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2021-08-06 09:26:02 +0900

    :lipstick:  Add adsense to aside

--- src/components/ReadingProgress/ProgressBar.tsx
+++ src/components/ReadingProgress/ProgressBar.tsx
@@ -1,10 +1,14 @@
 import React, { FC } from 'react'
 
-const ProgressBar: FC<{ max: number; val: number }> = ({ max, val }) => (
+const ProgressBar: FC<{ max: number; val: number; className?: string }> = ({
+  max,
+  val,
+  className
+}) => (
   <progress
     max={max}
     value={val}
-    className="appearance-none w-full fixed top-0 z-[1] lg:z-auto lg:w-56 inset-x-0 lg:inset-x-auto lg:sticky h-1 lg:bg-gray-200 lg:dark:bg-blue-gray-800 lg:top-1/2 lg:transform lg:rotate-90"
+    className={`appearance-none w-full h-1 lg:bg-gray-200 lg:dark:bg-blue-gray-800 ${className}`}
   />
 )
 
