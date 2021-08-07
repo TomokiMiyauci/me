5db95fea9e2de3f061a74033825d1c85282741ab
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2021-08-08 07:52:30 +0900

    :zap:  Reduce bundle size

--- src/types/firebase.ts
+++ src/types/firebase.ts
@@ -1,10 +1,10 @@
 import type { FirebaseApp } from 'firebase/app'
 import type { Auth } from 'firebase/auth'
-import type { FirebaseFirestore } from 'firebase/firestore/lite'
+import type { Firestore } from 'firebase/firestore/lite'
 
 type FirebaseState = Partial<{
   app: FirebaseApp
-  firestore: FirebaseFirestore
+  firestore: Firestore
   auth: Auth
 }>
 
