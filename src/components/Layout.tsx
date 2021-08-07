15a0f326defe7e98008e67bb1cbd8bf3f5fcb1ba
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2021-08-08 08:50:12 +0900

    :fire:  Disable algolia for bundle size

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -24,7 +24,7 @@ const Layout: FC<{
         currentPath={currentPath}
         originalPath={originalPath}
       />
-      <Search />
+      {/* <Search /> */}
 
       <Snackbar />
     </AuthContext.Provider>
