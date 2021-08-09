265ca17cc9bca2720f2178a962d23c62b96a5461
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2021-08-09 11:30:26 +0900

    :zap:  Reduce app bundle size
    Closes #142

--- src/hooks/firebase.ts
+++ src/hooks/firebase.ts
@@ -1,15 +1,10 @@
 import { useState, useContext } from 'react'
 import { getApps } from 'firebase/app'
-import { connectFirestoreEmulator } from 'firebase/firestore/lite'
-import { connectAuthEmulator } from 'firebase/auth'
 import { isLength0 } from '@miyauci/is-valid'
 import { pipe } from 'fonction'
-import { firebaseOptions } from '@/../config/constants'
 import FirebaseContext from '@/contexts/firebase'
 import { FirebaseState } from '@/types/firebase'
 import { isBrowser } from '@/utils/browser'
-import { initializeAuth, browserLocalPersistence } from 'firebase/auth'
-import { initializeFirestore } from 'firebase/firestore/lite'
 
 const notInitialized = pipe(getApps, isLength0)
 
@@ -17,23 +12,8 @@ const useFirebaseProvider = () => {
   const [firebase, setFirebase] = useState<FirebaseState>({})
 
   if (isBrowser && notInitialized()) {
-    import('firebase/app').then(async ({ initializeApp }) => {
-      const app = initializeApp(firebaseOptions)
-
-      const firestore = initializeFirestore(app, {})
-
-      const auth = initializeAuth(app, {
-        persistence: browserLocalPersistence
-      })
-
-      if (process.env.NODE_ENV === 'development') {
-        connectFirestoreEmulator(firestore, 'localhost', 8082)
-        connectAuthEmulator(auth, 'http://localhost:9099')
-      }
-
-      const { initializeFirebase } = await import('@/utils/firebase')
-      initializeFirebase(app)
-
+    import('@/utils/firebase').then(({ initializeFirebase }) => {
+      const { firestore, auth, app } = initializeFirebase()
       setFirebase({ app, firestore, auth })
     })
   }
--- src/utils/firebase.ts
+++ src/utils/firebase.ts
@@ -1,17 +1,46 @@
+import { initializeApp } from 'firebase/app'
 import { initializePerformance } from 'firebase/performance'
 import { initializeAnalytics, isSupported } from 'firebase/analytics'
-import type { FirebaseApp } from 'firebase/app'
+import {
+  initializeFirestore,
+  connectFirestoreEmulator
+} from 'firebase/firestore/lite'
+import {
+  initializeAuth,
+  browserLocalPersistence,
+  connectAuthEmulator
+} from 'firebase/auth'
+import { firebaseOptions } from '@/../config/constants'
 
-const initializeFirebase = (app: FirebaseApp): void => {
-  initializePerformance(app)
+import type { FirebaseState } from '@/types/firebase'
+
+const initializeFirebase = (): FirebaseState => {
+  const app = initializeApp(firebaseOptions)
+  const firestore = initializeFirestore(app, {})
+  const auth = initializeAuth(app, {
+    persistence: browserLocalPersistence
+  })
+
+  if (process.env.NODE_ENV === 'development') {
+    connectFirestoreEmulator(firestore, 'localhost', 8082)
+    connectAuthEmulator(auth, 'http://localhost:9099')
+  }
 
   if (process.env.NODE_ENV === 'production') {
+    initializePerformance(app)
+
     isSupported().then((e) => {
       if (e) {
         console.info('Initialize: analytics')
         initializeAnalytics(app)
       }
     })
+  }
+
+  return {
+    app,
+    firestore,
+    auth
   }
 }
 
