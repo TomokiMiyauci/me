1fd822bb018a639137f3e98fd33b7cade0f8e34f
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2021-08-12 09:38:16 +0900

    :page_facing_up:  Output license as another file

--- scripts/build.ts
+++ scripts/build.ts
@@ -11,7 +11,7 @@ build({
     'process.env.NODE_ENV': `"${process.env.NODE_ENV}"`
   },
   platform: 'browser',
-  legalComments: 'none'
+  legalComments: 'linked'
 })
 
 console.log(process.env.NODE_ENV)
