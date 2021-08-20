47529c9e9a043496d5a175a1fa74c994d13181a1
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2021-08-20 10:05:19 +0900

    :white_check_mark: (functions) Add test for meta-locales-[en|ja] functions

--- functions/src/meta/locales/ja/on_create.ts
+++ functions/src/meta/locales/ja/on_create.ts
@@ -1,40 +1,53 @@
 import { logger } from 'firebase-functions'
 
-import { createFunctions } from '@/util'
+import { createFunctions, switchable } from '@/util'
 import type { QueryDocumentSnapshot } from '@google-cloud/firestore'
 import type { MetaLocales } from '@/meta/locales/types'
 import admin from 'firebase-admin'
 
 const onCreate = createFunctions()
   .firestore.document('meta/{slug}/locales/ja')
-  .onCreate(async (snapShot: QueryDocumentSnapshot<Partial<MetaLocales>>) => {
-    const { title, description, thumbnailUrl, url } = snapShot.data()
+  .onCreate(
+    async (
+      snapShot: QueryDocumentSnapshot<Partial<MetaLocales>>
+    ): Promise<boolean> => {
+      const { title, description, thumbnailUrl, url } = snapShot.data()
 
-    if (!title || !description || !url) {
-      logger.error('title or description or url is not exists')
-      return
-    }
+      if (!title || !description || !url) {
+        logger.error('title or description or url is not exists')
 
-    const content: admin.messaging.MessagingPayload = {
-      notification: {
-        title,
-        body: description,
-        image: thumbnailUrl ?? ''
-      },
-
-      data: {
-        url,
-        locale: 'ja'
+        return false
       }
+
+      const content: admin.messaging.MessagingPayload = {
+        notification: {
+          title,
+          body: description,
+          image: thumbnailUrl ?? ''
+        },
+
+        data: {
+          url,
+          locale: 'ja'
+        }
+      }
+
+      const fn = switchable(
+        async () =>
+          admin
+            .messaging()
+            .sendToCondition("'article' in topics && 'ja' in topics", content)
+            .then(() => {
+              logger.log('Success sendToTopic', {
+                content
+              })
+              return true
+            }),
+        async () => true
+      )
+
+      return fn()
     }
-
-    await admin
-      .messaging()
-      .sendToCondition("'article' in topics && 'ja' in topics", content)
-
-    logger.log('Success sendToTopic', {
-      content
-    })
-  })
+  )
 
 export { onCreate }
