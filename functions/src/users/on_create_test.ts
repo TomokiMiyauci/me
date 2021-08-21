98a4fea2b3b139793244d54094ab7a5c7d43d520
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2021-08-21 19:48:36 +0900

    :recycle: (functions) Remove unused

--- functions/src/users/on_create_test.ts
+++ functions/src/users/on_create_test.ts
@@ -1,35 +1,27 @@
 import { onCreate as _onCreate } from '@/users/on_create'
 import { firestore } from 'firebase-admin'
-import { DocumentSnapshot, Timestamp } from '@google-cloud/firestore'
 import { test } from '@test/util'
 
-const UID = 'anonymous'
 const onCreate = test.wrap(_onCreate)
+const user = test.auth.exampleUserRecord()
 
 describe('onCreate', () => {
   afterAll(async () => {
-    test.cleanup()
-    await firestore().collection('users').doc(UID).delete()
+    await firestore().collection('users').doc(user.uid).delete()
   })
   it('save user info to firestore /document/users/{uid}', async () => {
-    const user = test.auth.exampleUserRecord()
-    user.uid = UID
     const timestamp = new Date('2021/1/1 00:01:02')
     await onCreate(user, {
       timestamp: timestamp.toISOString()
     })
 
-    const snapshot = (await firestore()
-      .collection('users')
-      .doc(UID)
-      .get()) as DocumentSnapshot<{ uid: string; createdAt: Timestamp }>
+    const snapshot = await firestore().collection('users').doc(user.uid).get()
+
     expect(snapshot.exists).toBeTruthy()
-    expect(snapshot.id).toBe(UID)
-    const uid = snapshot.data()?.uid
-    const createdAt = snapshot.data()?.createdAt
-    expect(uid).toBe(UID)
-    expect(
-      createdAt?.isEqual(firestore.Timestamp.fromDate(timestamp))
-    ).toBeTruthy()
+    expect(snapshot.id).toBe(user.uid)
+    expect(snapshot.data()).toEqual({
+      uid: user.uid,
+      createdAt: firestore.Timestamp.fromDate(timestamp)
+    })
   })
 })
