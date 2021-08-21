a2ee7b913eccc17dc543d57d53ec9a563b8a1f0e
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2021-08-21 20:15:03 +0900

    :fire: (functions) Remove unused

--- functions/src/fcm/on_create.ts
+++ functions/src/fcm/on_create.ts
@@ -72,40 +72,4 @@ const fcm = createFunctions()
     }
   )
 
-const onCreate = createFunctions()
-  .firestore.document('fcm/{token}')
-  .onCreate(async (snapshot: QueryDocumentSnapshot<Partial<FCM>>) => {
-    const { token, topics } = snapshot.data()
-
-    if (!token || !topics) {
-      logger.error('token is not exists.')
-      return
-    }
-
-    return Promise.all(
-      topics.map(async (topic) => {
-        await admin
-          .messaging()
-          .subscribeToTopic(token, topic)
-          .then(() => {
-            logger.info('Success token subscribeToTopic', {
-              token,
-              topic
-            })
-          })
-          .catch((e) =>
-            logger.error(e, {
-              token,
-              topic
-            })
-          )
-      })
-    ).then(() => {
-      logger.info('Success all topics subscription', {
-        token,
-        topics
-      })
-    })
-  })
-
-export { onCreate, fcm }
+export { fcm }
