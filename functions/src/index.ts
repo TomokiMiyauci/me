60b785fcd93b38fa5230f1fccca585488a3be6b0
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2021-08-21 19:49:52 +0900

    :fire: (functions) Remove unused

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -3,8 +3,6 @@ import { setup } from '@/util'
 setup()
 
 export * from '@/line'
-export * from '@/twitter'
-export * from '@/ping'
 export * from '@/fcm'
 export * from '@/meta'
 export * from '@/users'
