7e8a5b29db55a028a34332d49a654483d2be4cc6
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2021-08-30 19:59:05 +0900

    :bug:  Fix a export function name

--- src/workers/message.ts
+++ src/workers/message.ts
@@ -1,10 +1,12 @@
-import { signIn } from '@/workers/util/auth'
+import { getUser } from '@/workers/util/auth'
 import { auth } from '@/workers/util/firebase_init'
 
 declare var self: ServiceWorkerGlobalScope & typeof globalThis
 
 self.addEventListener('message', async ({ source }) => {
-  const user = await signIn(auth)
-  const _source = source as ServiceWorker
-  _source.postMessage(user.uid)
+  const user = await getUser(auth)
+  if (user) {
+    const _source = source as ServiceWorker
+    _source.postMessage(user.uid)
+  }
 })
