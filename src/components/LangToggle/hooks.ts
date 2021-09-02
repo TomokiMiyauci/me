f8ee0c87bf4f6315349d25c611370d202397c813
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2021-09-02 16:44:36 +0900

    :bug:  Remove gatsby-theme-i18n

--- src/components/LangToggle/hooks.ts
+++ src/components/LangToggle/hooks.ts
@@ -1,10 +1,8 @@
 import { Locale } from '@/../config/types'
 import { useMemo, useState } from 'react'
-import { useLocalization } from 'gatsby-theme-i18n'
 
 const useToggleLang = (initState?: Locale) => {
-  const { locale } = useLocalization()
-  const _initLocale = initState ?? (locale as Locale)
+  const _initLocale = initState
   const [enabled, setEnabled] = useState(_initLocale === 'ja')
 
   const lang = useMemo<Locale>(() => (enabled ? 'ja' : 'en'), [enabled])
