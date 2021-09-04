f3240eb6adf7d5cadcff9785c1f3fa1ede1d407f
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2021-09-04 21:05:01 +0900

    :children_crossing:  Add sequence logic to onClick

--- src/components/WebPush/TestWebPush.tsx
+++ src/components/WebPush/TestWebPush.tsx
@@ -2,17 +2,25 @@ import { defineComponent } from '@/utils/component'
 import { classNames } from '@/utils/class_name'
 import { useIsSupported } from '@/components/WebPush/hooks'
 import { useMemo } from 'react'
+import { useSequence } from '@/hooks/state'
 import { Icon } from '@iconify/react/dist/offline'
 import cancel from '@iconify-icons/mdi/cancel'
+import testIcon from '@iconify/icons-grommet-icons/test'
+import type { MouseEventHandler } from 'react'
 
 const TestWebPush = defineComponent<{
   onForeground: () => Promise<unknown> | unknown
   onBackground: () => Promise<unknown> | unknown
 }>(({ className, onForeground, onBackground }) => {
-  const { isPending, isRejected, isFulfilled } = useIsSupported()
+  const { isPending: isPendingSupported, isRejected } = useIsSupported()
+  const [isExecuting, sequence] = useSequence()
 
+  const isPending = useMemo<boolean>(
+    () => isPendingSupported || isExecuting,
+    [isPendingSupported, isExecuting]
+  )
   const placeholder = useMemo(() => {
-    if (isPending) return '...Loading'
+    if (isPendingSupported || isPending) return '...Loading'
     if (isRejected)
       return (
         <span className="space-x-2">
@@ -22,14 +30,19 @@ const TestWebPush = defineComponent<{
       )
 
     return 'background'
-  }, [isPending, isRejected])
+  }, [isPendingSupported, isPending, isRejected])
+
+  const handleClick: MouseEventHandler = () => sequence(onBackground)
 
   const baseClassName =
     'rounded-md p-2 flex-1 hover:bg-opacity-80 font-bold uppercase focus:ring transition duration-300 ring-gray-50 disabled:opacity-70 disabled:cursor-not-allowed'
 
   return (
     <div className={classNames(className)}>
-      <h4>Test</h4>
+      <h4 className="space-x-2">
+        <Icon className="animate-bounce" icon={testIcon} />
+        <span className="align-middle">Test</span>
+      </h4>
 
       <p className="text-gray-400">You can try it</p>
 
@@ -38,11 +51,11 @@ const TestWebPush = defineComponent<{
           onClick={onForeground}
           className={classNames(baseClassName, 'bg-teal-500')}
         >
-          forground
+          foreground
         </button>
         <button
-          disabled={!isFulfilled}
-          onClick={onBackground}
+          disabled={isPending}
+          onClick={handleClick}
           className={classNames(baseClassName, 'bg-fuchsia-500')}
         >
           {placeholder}
