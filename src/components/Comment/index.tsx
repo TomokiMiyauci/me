e4e6195743098cb23117d3818767f78b937bbf7c
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2021-09-04 15:50:55 +0900

    :zap:  Lazy load to utterances

--- src/components/Comment/index.tsx
+++ src/components/Comment/index.tsx
@@ -1,10 +1,31 @@
-import { FC, useMemo } from 'react'
-import { Utterances, Theme } from 'utterances-react-component'
+import Intersection from '@/components/Intersection'
+import { useMemo } from 'react'
 import { useDarkMode } from '@/hooks/dark_mode'
+import loadable from '@loadable/component'
+import { ProgressCircle } from '@/components/ProgressCircle/ProgressCircle'
+import delay from 'p-min-delay'
 
-const Comment: FC<{ className?: string }> = ({ className }) => {
+import type { FC, ReactNode } from 'react'
+import type { UtterancesProps, Theme } from 'utterances-react-component'
+
+const Utterances = loadable<UtterancesProps<ReactNode>>(
+  () =>
+    delay(
+      import('utterances-react-component').then(({ Utterances }) => Utterances),
+      1000
+    ),
+  {
+    fallback: (
+      <div className="min-h-[269px] flex justify-center items-center">
+        <ProgressCircle />
+      </div>
+    )
+  }
+)
+
+const Index: FC<{ className?: string }> = ({ className }) => {
   const { value } = useDarkMode()
-  const theme = useMemo(
+  const theme = useMemo<Theme>(
     () => (value ? 'icy-dark' : 'github-light') as Theme,
     [value]
   )
@@ -14,14 +35,18 @@ const Comment: FC<{ className?: string }> = ({ className }) => {
       <h3 id="comment" className={`p-4 text-3xl ${className}`}>
         Comments
       </h3>
-      <Utterances
-        repo="TomokiMiyauci/me"
-        issueTerm="pathname"
-        label="comment"
-        theme={theme}
-      />
+      <div className="min-h-[269px]">
+        <Intersection keepRender>
+          <Utterances
+            repo="TomokiMiyauci/me"
+            issueTerm="pathname"
+            label="comment"
+            theme={theme}
+          />
+        </Intersection>
+      </div>
     </>
   )
 }
 
-export default Comment
+export default Index
