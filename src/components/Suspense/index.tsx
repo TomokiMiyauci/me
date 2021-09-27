8f81b3dcd5acf2a3d6c1cdca5a77db88d1323e4d
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2021-09-28 00:03:48 +0900

    :children_crossing:  Load before rendering lazy load component

--- src/components/Suspense/index.tsx
+++ src/components/Suspense/index.tsx
@@ -1,16 +1,25 @@
-import { useState, useEffect } from 'react'
-import type { FC } from 'react'
+import { useState, useEffect, useRef } from 'react'
+import { LoadableComponent } from '@loadable/component'
+import type { FC, ReactElement } from 'react'
 const Suspense: FC<{
-  children: JSX.Element
+  children: ReactElement<any, LoadableComponent<{}>>
   fallback: JSX.Element
   delay?: number
 }> = ({ fallback, children, delay = 1000 }) => {
   const [show, setShow] = useState(false)
+  const timeoutId = useRef<NodeJS.Timeout>()
 
   useEffect(() => {
-    setTimeout(() => {
+    timeoutId.current = setTimeout(async () => {
+      await children.type?.load?.()
       setShow(true)
     }, delay)
+
+    return () => {
+      if (timeoutId.current) {
+        clearTimeout(timeoutId.current)
+      }
+    }
   }, [])
 
   return show ? children : fallback
