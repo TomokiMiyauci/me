8d8d6cb830e50c4cd8ae75c41a932f1ef4304b42
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2021-09-30 12:19:38 +0900

    :sparkles:  Add shortcut for translate

--- src/components/LangSwitcher/ButtonLangSwitcher/index.tsx
+++ src/components/LangSwitcher/ButtonLangSwitcher/index.tsx
@@ -1,12 +1,18 @@
 import ButtonLangSwitcher from '@/components/LangSwitcher/ButtonLangSwitcher/ButtonLangSwitcher'
 import { useContext } from 'react'
+import { useEventListener } from '@/hooks/event_listener'
 import Context from '@/components/LangSwitcher/context'
 import type { FC } from 'react'
 
 const Index: FC = () => {
   const [_, changeShow] = useContext(Context)
+  useEventListener('keydown', ({ metaKey, code }) => {
+    if (metaKey && code === 'KeyI') {
+      changeShow(true)
+    }
+  })
   return (
-    <span className="tooltip" data-tooltip="Translate">
+    <span className="tooltip" data-tooltip="Translate ⌘I">
       <ButtonLangSwitcher
         onClick={() => {
           changeShow(true)
