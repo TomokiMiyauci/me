f391c11f3137f69fcfb65f2f9fa6864ff0feddc9
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2021-09-30 23:42:55 +0900

    :children_crossing:  Add transition to accent color pallet

--- src/components/AccentColor/AccentColor.tsx
+++ src/components/AccentColor/AccentColor.tsx
@@ -3,10 +3,21 @@ import Esc from '@/components/Esc'
 import Tooltip from '@/components/Tooltip'
 import IconSkeltonLoader from '@/components/Icon/IconSkeltonLoader'
 import Context from '@/components/AccentColor/context'
-
+import { Transition } from '@headlessui/react'
 import { useAccentColor } from '@/utils/use_accent_color'
 import { useContext } from 'react'
+import { classNames } from '@/utils/class_name'
 import type { FC } from 'react'
+
+const delayMap: Record<number, string> = {
+  0: 'delay-100',
+  1: 'delay-300',
+  2: 'delay-500',
+  3: 'delay-700',
+  4: 'delay-[900ms]',
+  5: 'delay-[1100ms]',
+  6: 'delay-[1300ms]'
+}
 
 const AccentColor: FC = () => {
   const { switchColor, colorPalette } = useAccentColor()
@@ -47,8 +58,15 @@ const AccentColor: FC = () => {
       <hr className="border-gray-200 dark:border-blue-gray-700" />
 
       <section className="flex-1 grid grid-cols-2 md:grid-cols-3 place-items-center">
-        {colorPalette.map(({ label, color }) => (
-          <div key={label} className="text-center">
+        {colorPalette.map(({ label, color }, i) => (
+          <Transition
+            enterFrom="scale-0"
+            enter={classNames('transition-transform duration-500', delayMap[i])}
+            appear
+            show
+            key={label}
+            className="text-center"
+          >
             <button
               title={label}
               onClick={() => {
@@ -56,14 +74,19 @@ const AccentColor: FC = () => {
                 changeShow(false)
               }}
               aria-label={`Switch accent color to ${label}`}
-              className="hover:scale-[1.2] transform duration-500 w-20 h-20 md:w-24 md:h-24 animate-pulse-bit-slow transition-transform rounded-full"
-              style={{ backgroundColor: color }}
-            />
+              className="relative hover:scale-[1.2] hover:animate-none transform duration-300 w-20 h-20 md:w-24 md:h-24 animate-pulse-bit-slow transition-transform"
+            >
+              <div className="absolute inset-0 blur gradation rounded-full" />
+              <div
+                style={{ backgroundColor: color }}
+                className="relative h-full w-full rounded-full"
+              />
+            </button>
 
             <h3 className="capitalize text-lg font-bold" style={{ color }}>
               {label}
             </h3>
-          </div>
+          </Transition>
         ))}
       </section>
     </CardDialog>
