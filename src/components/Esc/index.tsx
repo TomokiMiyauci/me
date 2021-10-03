6b567412881b250385b820fc61128ea9d8512009
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2021-10-03 21:28:46 +0900

    :recycle:  Use external useShortcut hooks

--- src/components/Esc/index.tsx
+++ src/components/Esc/index.tsx
@@ -1,27 +1,42 @@
 import loadable from '@loadable/component'
 import delay from 'p-min-delay'
 import SkeltonLoader from '@/components/SkeltonLoader/SkeltonLoader'
+import Tooltip from '@/components/Tooltip'
+import { useShortcut } from 'react-hookable'
+import { useSafeLogEvent } from '@/hooks/firebase/analytics'
+import type { FC } from 'react'
 const Esc = loadable(() => delay(import('@/components/Esc/Esc'), 1000), {
   ssr: false,
   fallback: (
     <SkeltonLoader className="border dark:border-blue-gray-700 inline-block align-middle w-[35.53px] h-[26px] rounded-md" />
   )
 })
-import Tooltip from '@/components/Tooltip'
-import { useShortcut } from '@/hooks/event_listener'
-import type { FC } from 'react'
 
 const Index: FC<
   JSX.IntrinsicElements['button'] & {
     onKeyDownEscape?: (ev: globalThis.KeyboardEvent) => void
   }
 > = ({ onKeyDownEscape, ...props }) => {
+  const { safeLogEvent } = useSafeLogEvent()
+
   useShortcut(
     {
-      code: 'Escape'
+      key: 'Escape'
     },
-    (ev) => onKeyDownEscape?.(ev),
-    []
+    (ev) => {
+      onKeyDownEscape?.(ev)
+      const { code, metaKey, shiftKey, ctrlKey, key, altKey } = ev
+      safeLogEvent((analytics, logEvent) =>
+        logEvent(analytics, 'shortcut', {
+          code,
+          metaKey,
+          shiftKey,
+          ctrlKey,
+          key,
+          altKey
+        })
+      )
+    }
   )
 
   return (
--- src/hooks/event_listener.ts
+++ src/hooks/event_listener.ts
@@ -1,7 +1,6 @@
 import { useEffect } from 'react'
 import { makeEventListenerPair } from '@/utils/event_listener'
 import { isBrowser } from '@/utils/environment'
-import { useSafeLogEvent } from '@/hooks/firebase/analytics'
 
 import type { DependencyList } from 'react'
 
@@ -27,37 +26,4 @@ const useKeydown = (
   deps?: DependencyList
 ): void => useEventListener('keydown', handler, deps)
 
-type KeyMap = Pick<
-  KeyboardEvent,
-  'key' | 'metaKey' | 'code' | 'altKey' | 'ctrlKey' | 'shiftKey' | 'repeat'
->
-const useShortcut = (
-  keyMap: Partial<KeyMap>,
-  handler: (ev: KeyboardEvent) => void,
-  deps?: DependencyList
-): void => {
-  const { safeLogEvent } = useSafeLogEvent()
-
-  const fn = (ev: KeyboardEvent): void => {
-    const isValid = Object.entries(keyMap).every(
-      ([key, value]) => ev[key as keyof KeyMap] === value
-    )
-    if (!isValid) return
-    handler(ev)
-    safeLogEvent((analytics, logEvent) => {
-      const { code, metaKey, shiftKey, ctrlKey, key, altKey } = ev
-      logEvent(analytics, 'shortcut', {
-        code,
-        metaKey,
-        shiftKey,
-        ctrlKey,
-        key,
-        altKey
-      })
-    })
-  }
-
-  useKeydown(fn, deps)
-}
-
-export { useEventListener, useKeydown, useShortcut }
+export { useEventListener, useKeydown }
